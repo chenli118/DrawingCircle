e808df51c8f9614a66192142c60874e1201197c7
chenli <chenli_mail@163.com>
2019-08-12 15:47:10 +0800

    添加项目文件。

---
+++ DrawingCircle/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingCircle
+{
+    public partial class Form1 : Form
+    {
+        private Font _font = new Font("Arial", 12);
+
+        private Brush _brush = new SolidBrush(Color.Black);
+
+        private Pen _pen = new Pen(Color.Black, 1f);
+
+        private string _text = "角度字";
+
+        int wX = 600;
+        int wY = 600;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += Form1_Paint;
+
+            Point x = this.ClientRectangle.Location;
+
+            this.ClientSize = new Size(wX, wY);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {           
+
+            Point center = new Point(wX/2, wY/2);
+            Point edge = new Point(
+              (int)(center.X + wX/4 * Math.Cos(Math.PI * 30 / 180.0)),
+              (int)(center.Y + wY/4 * Math.Sin(Math.PI * 120 / 180.0)));
+            e.Graphics.DrawLine(Pens.Black, center, edge);
+            GraphicsText graphicsText = new GraphicsText();
+            graphicsText.Graphics = e.Graphics;
+            e.Graphics.DrawEllipse(Pens.Black, new Rectangle(new Point(10, 10), new Size(450, 450)));
+            e.Graphics.DrawEllipse(Pens.Black, new Rectangle(new Point(85, 85), new Size(300, 300)));
+            // 绘制围绕点旋转的文本
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+            format.LineAlignment = StringAlignment.Center;
+            graphicsText.DrawString(_text, _font, _brush, new PointF(300, 80), format, 160f);
+            graphicsText.DrawString(_text, _font, _brush, new PointF(300, 160), format, 30f);
+        } 
+    }
+}
